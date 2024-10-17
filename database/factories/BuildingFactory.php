7fe93c187c86753a9b61763256c9fda5243a6f8f
Laptop-Salad <awallis1609@gmail.com>
2024-10-18 00:43:58 +0100

    feat: buildings table

---
+++ database/factories/BuildingFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Street;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Building>
+ */
+class BuildingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'number' => random_int(1, 100),
+            'street_id' => Street::factory(),
+        ];
+    }
+}
---
+++ database/migrations/2024_10_17_234211_create_buildings_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Street;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('buildings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('number');
+            $table->foreignIdFor(Street::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('buildings');
+    }
+};
